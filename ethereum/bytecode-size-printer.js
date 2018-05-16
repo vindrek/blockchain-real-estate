36b1a4325be0fd0cb39c850bff775a740955ddee
Indrek <indrek.varva@gmail.com>
2018-05-17 00:36:25 +0300

    npm script to run contract bytecode size evaluator

--- ethereum/bytecode-size-printer.js
+++ ethereum/bytecode-size-printer.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
+/* Code adapted from the implementation proposed by user Chet (https://ethereum.stackexchange.com/a/41502) */
 function sizes(name) {
     var abi = JSON.parse(fs.readFileSync("build/contracts/" + name + ".json", 'utf8'));
     var size = (abi.bytecode.length / 2) - 1;
@@ -8,7 +9,7 @@ function sizes(name) {
 }
 
 function fmt(obj) {
-    return `${obj.name} ${obj.size} ${obj.deployedSize}`;
+    return `${obj.name} constructor bytecode size: ${obj.size} ; deployed bytecode size: ${obj.deployedSize}`;
 }
 
 var l = fs.readdirSync("build/contracts");
